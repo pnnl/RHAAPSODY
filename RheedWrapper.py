f36b1c589c1cc873624129755f0e2c5ab9df57f7
HenrySprueill <henry.sprueill@pnnl.gov>
2025-02-13 10:32:30 -0800

    update RheedWrapper.py

--- RheedWrapper.py
+++ RheedWrapper.py
@@ -1,10 +1,3 @@
-import os
-import sys
-import math
-import json
-import time
-import gc
-from ZMQMessenger import ZMQMessenger
 """Disclaimer
 
 This material was prepared as an account of work sponsored by an agency of the United States Government.  Neither the United States Government nor the United States Department of Energy, nor Battelle, nor any of their employees, nor any jurisdiction or organization that has cooperated in the development of these materials, makes any warranty, express or implied, or assumes any legal liability or responsibility for the accuracy, completeness, or usefulness or any information, apparatus, product, software, or process disclosed, or represents that its use would not infringe privately owned rights.
@@ -16,8 +9,15 @@ for the
 UNITED STATES DEPARTMENT OF ENERGY
 under Contract DE-AC05-76RL01830
 """
+
+import os
+import sys
+import time
+import gc
+from ZMQMessenger import ZMQMessenger
+
 # disable tensorflow warnings
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 
 sys.path.append(".")
 from embeddings import EmbeddingModel
@@ -30,8 +30,10 @@ root_directory = "c:\\"
 Rheed_subscriber = None
 
 # pnl_server='WE37491.pnl.gov'
-pnl_server='130.20.173.71'
-labnet_server='WE27790.labnet.pnl.gov'
+# pnl_server='130.20.173.71'
+pnl_server = "130.20.93.110"
+labnet_server = "WE27790.labnet.pnl.gov"
+
 
 def ConnectRheed(publisher_port, subscriber_port, subscriber_ip):
     global Rheed_subscriber
@@ -40,6 +42,7 @@ def ConnectRheed(publisher_port, subscriber_port, subscriber_ip):
     Rheed_publisher = ZMQMessenger("localhost", publisher_port, "PUB", "rheed")
     Rheed_subscriber = ZMQMessenger(subscriber_ip, subscriber_port, "SUB", "rheed")
     Rheed_publisher.CreateSocketPair(Rheed_subscriber)
+
 
 def MessageHandler(header):
     print("PyJEM handle message")
@@ -53,12 +56,13 @@ def MessageHandler(header):
             return
 
         # clear queue if called
-        if header['message']['msgType']=='Reset':
+        if header["message"]["msgType"] == "Reset":
+            print("Trying to reset...")
             Rheed_publisher.ClearQueue()
-            print('QUEUE CLEARED')
+            print("QUEUE CLEARED")
             return
 
-        # call to analysis 
+        # call to analysis
         reply_header = rheed_analysis.receive_message(header)
 
         # create the return message
@@ -68,13 +72,13 @@ def MessageHandler(header):
         # send the return message
         Rheed_publisher.SendHeader(reply_header, 0)
 
-
     except Exception as e:
         print(e)
 
 
 def Version():
     return "1/30/24"
+
 
 def main():
 
@@ -88,30 +92,37 @@ def main():
     count = 0
     while True:
 
+        # header = Rheed_subscriber.GetHeader("Reset")
+        # if header is not None:
+        #    MessageHandler(header)
+        #    continue
+
         header = Rheed_subscriber.GetHeader("RawImage")
 
         while header is not None:
             MessageHandler(header)
             header = Rheed_subscriber.GetHeader("RawImage")
-            count+=1
-            if count%10==0:
+            count += 1
+            if count % 10 == 0:
                 gc.collect()
 
-        time.sleep(.5)
+        time.sleep(0.5)
 
 
 if __name__ == "__main__":
     gc.disable()
-    
+
     # initialize analysis
     rheed_analysis = AutoRHEEDer(
-            root="/mnt/atscale/TestExp",
-            data_processor=EmbeddingModel(),
-            change_detector=ChangepointDetection(cost_threshold=0.05, window_size=300, min_time_between_changepoints=10),
-            classifier=GraphClustering(resolution=1, seed=123),
-            segmenter=Segment(),
-            starting_period=30,
-            max_embeddings=2101,
-            max_steps=2100,
-            )
+        root="/mnt/atscale/TestExp",
+        data_processor=EmbeddingModel(),
+        change_detector=ChangepointDetection(
+            cost_threshold=0.025, window_size=300, min_time_between_changepoints=10
+        ),
+        classifier=GraphClustering(resolution=1, seed=123),
+        segmenter=Segment(),
+        starting_period=30,
+        max_embeddings=2101,
+        max_steps=2100,
+    )
     main()
